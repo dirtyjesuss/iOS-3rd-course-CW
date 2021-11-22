23a8d0969dfb4bfd762bcfc411296c2795c18e67
Ruslan Khanov <hanov@technokratos.com>
2021-11-22 18:24:32 +0300

    initial project

---
+++ CoordinationControlWorkApp/CoordinationControlWorkApp/Scenes/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  CoordinationControlWorkApp
+//
+//  Created by Ruslan Khanov on 22.11.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
